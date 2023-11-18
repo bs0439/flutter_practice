15530af437be7c79c728e359c7c36cd2f76caf01
beomsu <kbs0439@gmail.com>
2023-11-19 00:58:43 +0900

    study constructor

--- const_constructor/lib/screen/home_screen.dart
+++ const_constructor/lib/screen/home_screen.dart
@@ -11,7 +11,42 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("test"),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TestWidget(label: "test1"),
+            TestWidget(label: "test2"),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('빌드!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  final String label;
+
+  const TestWidget({
+    required this.label,
+    Key? key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print('$label 빌드 실행');
+
+    return Container(
+      child: Text(
+        label,
+      ),
     );
   }
 }
